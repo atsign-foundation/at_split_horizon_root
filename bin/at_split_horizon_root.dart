2f42ffc1c7e2db3e019ce4288114a30335b4a301
colin constable <colin@constable.cc>
2022-11-04 19:01:33 -0700

    feat: First MVP release

---
+++ bin/at_split_horizon_root.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:at_lookup/at_lookup.dart';
+import 'package:hive/hive.dart';
+
+
+void main() async {
+  var box = await Hive.openBox('rootBox', path: './');
+
+  await loadBox(box);
+
+  var secCon = SecurityContext();
+  secCon.useCertificateChain('fullchain.pem');
+  secCon.usePrivateKey('privkey.pem');
+  secCon.setTrustedCertificates('cacert.pem');
+
+  await SecureServerSocket.bind(InternetAddress.anyIPv4, 64, secCon, requestClientCertificate: false)
+      .then((SecureServerSocket secSocket) {
+    secSocket.listen((connection) {
+      var send = utf8.encode("@");
+      connection.add(send);
+      connection.listen((Uint8List data) async {
+        final message = String.fromCharCodes(data);
+        if (message.trim() == '' || message.length>257) {
+          connection.destroy();
+        }
+        var stuff = box.get(message.trim());
+        if (stuff == null) {
+          connection.writeln(await rootLookup(message));
+        } else {
+          connection.writeln(stuff);
+        }
+        var send = utf8.encode("@");
+        connection.add(send);
+      }, onError: (error) {
+        print(error.toString());
+      });
+    });
+  });
+}
+
+Future<String> rootLookup(String atsign) async {
+  var atLookupImpl = AtLookupImpl('', 'root.atsign.org', 64);
+  SecondaryAddress secondaryAddress;
+  SecondaryAddressFinder secondaryAddressFinder;
+
+  try {
+    secondaryAddressFinder = atLookupImpl.secondaryAddressFinder;
+
+    secondaryAddress = await secondaryAddressFinder.findSecondary(atsign);
+  } catch (e) {
+    return ('null');
+  }
+
+  return ('${secondaryAddress.host}:${secondaryAddress.port}');
+}
+
+
+/// TODO  Catch SIGHUP and reload DB for updates to file
+/// Find out why we need dummy data else the first put does not work ??
+/// 
+Future<void> loadBox(Box box) async {
+  box.clear();
+  File file = File('./atServers');
+
+  List<String> lines = file.readAsLinesSync();
+  // Why is this needed ?
+  await box.put('dummy', 'data');
+  try {
+    for (var line in lines) {
+      var atsign = line.split(RegExp(r'\s+'));
+      await box.put(atsign[0], atsign[1]);
+    }
+  } catch (e) {
+    print(e.toString());
+    print('Format error in etc/atsigns file');
+    exit(-1);
+  }
+}
---
+++ test/at_split_horizon_root_test.dart
@@ -0,0 +1,8 @@
+import 'package:at_split_horizon_root/at_split_horizon_root.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
