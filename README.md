dadc670cc76edabe55510470c48dfe5005ec7773
Colin Constable <colin@constable.cc>
2022-11-07 19:02:03 -0800

    feat: Documentation readme
    docs

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+<img width=250px src="https://atsign.dev/assets/img/atPlatform_logo_gray.svg?sanitize=true">
+
+# atSplitHorizonRoot aka shrd
+
+TLDR;
+Some times you might want to runn your own atDirectory (root server), perhaps your network is not connected to the Internet or maybe Internet access is not always available.
+If that is the case, shrd is the solution. Your atPlatform code can point at the shrd and it will answer with your atServers locations on your network. If the atSign being looked up is not in your config then it will be forwarded to the Internet and resolved, if the Internet is not available then the lookup will fail sending back a 'null'.
+
+## Why is this called Split Horizon Root ?
+DNS has for years had the same functionality for DNS lookups and this has been called split horizon DNS. In fact you may want to run a DNS sever in split horizon mode in conjuction with shrd!
+
+
+## Quick tour
+
+There is just one binary:-
+
+`shrd` : The atSplitHorizonRoot daemon. 
+
+
+
+## Usage
+
+### shrd (daemon)
+Run the daemon binary file or the dart file:
+```sh
+./shrd <args|flags>
+```
+```sh
+dart run bin/sshnpd.dart <args|flags>
+```
+
+| Argument        | Abbreviation | Mandatory | Description                                                                         |    Default    |
+|-----------------|--------------|-----------|-------------------------------------------------------------------------------------|---------------|
+| --port          | -p           | false     | TCP port number shrd listens on                                                     |      64       |
+| --config        | -c           | false     | Configuration file for local atServers                                              |   atServers   |
+| --ssl-fullchain | -f           | false     | SSL fullchain in PEM format                                                         | fullchain.pem |
+| --ssl-privkey   | -k           | false     | SSL private key in PEM format                                                       |  privkey.pem  |
+
+| Flags               | Abbreviation | Description                                                                     |
+|---------------------|--------------|---------------------------------------------------------------------------------|
+| --[no-]verbose      | -v           | More logging                                                                    |
+
+
+## Who is this tool for?
+
+System Admins  
+Network Admins  
+Network Manufacturers
+
+
+## Maintainers
+
+Created by Atsign 
+
+Original code by [@cconstab](https://github.com/cconstab)
+
--- bin/at_split_horizon_root.dart
+++ bin/at_split_horizon_root.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'dart:convert';
 import 'dart:typed_data';
+// external packages
+import 'package:args/args.dart';
 import 'package:logging/src/level.dart';
 
 // @platform packages
@@ -8,23 +10,75 @@ import 'package:at_utils/at_logger.dart';
 import 'package:at_lookup/at_lookup.dart';
 import 'package:hive/hive.dart';
 
-void main() async {
+// Local packages
+import 'package:at_split_horizon_root/home_directory.dart';
+import 'package:at_split_horizon_root/check_file_exists.dart';
+
+void main(List<String> args) async {
+  int port = 64;
+  String atServers = 'atServers';
+  String fullChain = 'fullchain.pem';
+  String privKey = 'privkey.pem';
   final AtSignLogger logger = AtSignLogger(' shr ');
 
-  var box = await Hive.openBox('rootBox', path: './');
+  // Have an argumnent :-)
+  var parser = ArgParser();
+  // Basic arguments
+  parser.addOption('port',
+      abbr: 'p',
+      mandatory: false,
+      defaultsTo: '64',
+      help: 'TCP port number to listen on');
+  parser.addOption('config',
+      abbr: 'c',
+      mandatory: false,
+      defaultsTo: 'atServers',
+      help: 'configuration file for local atServers');
+  parser.addOption('ssl-fullchain',
+      abbr: 'f',
+      mandatory: false,
+      defaultsTo: 'fullchain.pem',
+      help: 'SSL certificate file in pem format');
+  parser.addOption('ssl-privkey',
+      abbr: 'k',
+      mandatory: false,
+      defaultsTo: 'privkey.pem',
+      help: 'SSL private key file in pem format');
+  // Flags
+  parser.addFlag('verbose', abbr: 'v', help: 'More logging');
 
-  await loadBox(box);
+  // Get args into variables
+  try {
+    var results = parser.parse(args);
+    port = int.parse(results['port']);
+    atServers = results['config'];
+    fullChain = results['ssl-fullchain'];
+    privKey = results['ssl-privkey'];
+
+    // Get logging at the right level -v = INFO
+    logger.logger.level = Level.SEVERE;
+    if (results['verbose']) {
+      logger.logger.level = Level.INFO;
+    }
+  } catch (e) {
+    print(e.toString());
+    print(parser.usage);
+    exit(1);
+  }
+
+  // Get hiveDB up and running
+  var box = await Hive.openBox('rootBox', path: './');
+  await loadBox(box, atServers, logger);
 
   // re-load hiveDB if we see a SIGHUP
   ProcessSignal.sighup.watch().listen((signal) async {
-    await loadBox(box);
-    logger.info('reloadingbox');
+    await loadBox(box, atServers, logger);
+    logger.info(' caught SIGHUP reloading atServer Database');
   });
 
   var secCon = SecurityContext();
-  secCon.useCertificateChain('fullchain.pem');
-  secCon.usePrivateKey('privkey.pem');
-  secCon.setTrustedCertificates('cacert.pem');
+  secCon.useCertificateChain(fullChain);
+  secCon.usePrivateKey(privKey);
 
   await SecureServerSocket.bind(InternetAddress.anyIPv4, 64, secCon,
           requestClientCertificate: false)
@@ -34,14 +88,16 @@ void main() async {
       connection.add(send);
       connection.listen((Uint8List data) async {
         final message = utf8.decode(data);
-        if (message.trim() == '' || message.length > 257) {
+        String atsign = message.trim();
+        if (atsign == '' || atsign.length > 255) {
           connection.destroy();
         }
-        var stuff = box.get(message.trim());
+        var stuff = box.get(atsign);
         if (stuff == null) {
-          connection.writeln(await rootLookup(message.trim(), logger));
+          connection.writeln(await rootLookup(atsign, logger));
         } else {
           connection.writeln(stuff);
+          logger.info(' Local lookup for: $atsign');
         }
         var send = utf8.encode("@");
         connection.add(send);
@@ -56,7 +112,7 @@ Future<String> rootLookup(String atsign, AtSignLogger logger) async {
   var atLookupImpl = AtLookupImpl('', 'root.atsign.org', 64);
   SecondaryAddress secondaryAddress;
   SecondaryAddressFinder secondaryAddressFinder;
-  logger.info('Lookup for: $atsign');
+  logger.info(' Upstream lookup for: $atsign');
   try {
     secondaryAddressFinder = atLookupImpl.secondaryAddressFinder;
 
@@ -68,20 +124,40 @@ Future<String> rootLookup(String atsign, AtSignLogger logger) async {
   return ('${secondaryAddress.host}:${secondaryAddress.port}');
 }
 
-Future<void> loadBox(Box box) async {
+Future<void> loadBox(Box box, String atServers, AtSignLogger logger) async {
   await box.clear();
-  File file = File('./atServers');
+  File file = File(atServers);
+  bool fileError = false;
 
+// test atServer file for errors
   List<String> lines = file.readAsLinesSync();
   try {
     for (var line in lines) {
-      var atsign = line.split(RegExp(r'\s+'));
-      await box.put(atsign[0], atsign[1]);
+      // Allow Comments with leading #
+      if (!line.trimLeft().startsWith('#')) {
+        line.split(RegExp(r'\s+'));
+      }
     }
   } catch (e) {
-    print(e.toString());
-    print('Format error in atServers file');
-    await box.close();
-    exit(-1);
+    logger.info(e.toString());
+    logger.info('Format error in atServers file');
+    fileError = true;
+  }
+
+  if (!fileError) {
+    try {
+      for (var line in lines) {
+        // Allow Comments with leading #
+        if (!line.trimLeft().startsWith('#')) {
+          var atsign = line.split(RegExp(r'\s+'));
+          await box.put(atsign[0], atsign[1]);
+        }
+      }
+    } catch (e) {
+      logger.severe(e.toString());
+      logger.severe('Format error in atServers file');
+      await box.close();
+      exit(-1);
+    }
   }
 }
